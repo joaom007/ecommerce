ae84a841d449d796d17ee7c7bf0fd8c32eee3aa8
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2020-09-11 13:10:35 -0300

    Updating in the customer registration form

--- src/components/FormCustomer/index.js
+++ src/components/FormCustomer/index.js
@@ -1,25 +1,163 @@
 import React from "react";
-import { Formik, Field, Form } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import Button from "../Button";
+import {
+  FlexContainer,
+  GridContainer,
+  Title,
+  StyledTerms,
+  DivBackDrop,
+} from "./styles";
 
-const FormCustomer = (props) =>
-  props.primary ? (
-    <Formik
-      initialValues={{ name: "", email: "", cpf: "", phone: "" }}
-      onSubmit={async (values) => {
-        await new Promise((resolve) => setTimeout(resolve, 500));
+const FormCustomer = () => (
+  <Formik
+    initialValues={{
+      name: "",
+      email: "",
+      cpf: "",
+      rg: "",
+      phone: "",
+      birthday: "",
+      password: "",
+      district: "",
+      number: "",
+      street: "",
+      city: "",
+      state: "",
+      zipCode: "",
+    }}
+    validationSchema={Yup.object({
+      name: Yup.string()
+        .max(40, "O nome deve ter 40 caracteres ou mais")
+        .required("*Obrigatório"),
+      email: Yup.string().email("Email inválido").required("*Obrigatório"),
+      cpf: Yup.string()
+        .max(11, "O número máximo de caracteres e 11")
+        .required("*Obrigatório"),
+      phone: Yup.string()
+        .max(
+          11,
+          "o número excede o valor máximo permitido de caracteres de 11 dígitos"
+        )
+        .required("*Obrigatório"),
+      rg: Yup.string()
+        .max(9, "Número máximo excedido")
+        .required("* campo obrigatório"),
+      birthday: Yup.date().required("* campo data obrigatório"),
+      district: Yup.string().required("* campo obrigatório"),
+      password: Yup.string().required("* Obrigatório"),
+      number: Yup.string()
+        .max(5, "Número máximo de 5 carácteres")
+        .required("* Obrigatório"),
+      street: Yup.string()
+        .max(100, "Número máximo de 100 caracteres excedido")
+        .required(),
+      city: Yup.string()
+        .max(45, "Número máxido de 45 caractere excedido")
+        .required("* Campo obrigatório"),
+      state: Yup.string()
+        .max(2, "São permitidos apenas dois caracteres")
+        .required("* Campo obrigatório"),
+      zipCode: Yup.string()
+        .max(9, "Número máxido de 9 caracteres excedido")
+        .required("* Campo obrigatório"),
+      acceptedTerms: Yup.boolean()
+        .required("*Obrigatório")
+        .oneOf([true], "Você deve aceitar os termos e condições."),
+    })}
+    onSubmit={(values, { setSubmitting }) => {
+      setTimeout(() => {
         alert(JSON.stringify(values, null, 2));
-      }}
-    >
-      <Form>
-        <Field name="name" type="text" />
-        <Field name="email" type="email" />
-        <Field name="cpf" type="text" />
-        <Field name="phone" type="phone" />
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
-  ) : (
-    <h1>Pessoa Juridica</h1>
-  );
+        setSubmitting(false);
+      }, 400);
+    }}
+  >
+    <Form>
+      <DivBackDrop></DivBackDrop>
+      <GridContainer>
+        <Title className="title">Pessoa Física</Title>
+        <FlexContainer className="name">
+          <label htmlFor="name">Nome:</label>
+          <Field name="name" type="text" />
+          <ErrorMessage name="name" />
+        </FlexContainer>
+        <FlexContainer className="email">
+          <label htmlFor="email">Email:</label>
+          <Field name="email" type="email" />
+          <ErrorMessage name="name" />
+        </FlexContainer>
+        <FlexContainer className="cpf">
+          <label htmlFor="cpf">CPF:</label>
+          <Field name="cpf" type="text" />
+          <ErrorMessage name="cpf" />
+        </FlexContainer>
+        <FlexContainer className="rg">
+          <label htmlFor="rg">RG:</label>
+          <Field name="rg" type="text" />
+          <ErrorMessage name="rg" />
+        </FlexContainer>
+        <FlexContainer className="phone">
+          <label htmlFor="phone">Telefone:</label>
+          <Field name="phone" type="text" />
+          <ErrorMessage name="phone" />
+        </FlexContainer>
+        <FlexContainer className="bithday">
+          <label htmlFor="birthday">Data de Nascimento:</label>
+          <Field name="birthday" type="date" />
+          <ErrorMessage name="birthday" />
+        </FlexContainer>
+        <FlexContainer className="password">
+          <label htmlFor="password">Senha:</label>
+          <Field name="password" type="password" />
+          <ErrorMessage name="password" />
+        </FlexContainer>
+        <Title className="address">Endereço:</Title>
+        <FlexContainer className="district">
+          <label htmlFor="district">Bairro:</label>
+          <Field name="district" type="text" />
+          <ErrorMessage name="district" />
+        </FlexContainer>
+        <FlexContainer className="street">
+          <label htmlFor="street">Rua:</label>
+          <Field name="street" type="text" />
+          <ErrorMessage name="street" />
+        </FlexContainer>
+        <FlexContainer className="number">
+          <label htmlFor="number">Número:</label>
+          <Field name="number" type="text" />
+          <ErrorMessage name="number" />
+        </FlexContainer>
+        <FlexContainer className="city">
+          <label htmlFor="city">Cidade:</label>
+          <Field name="city" type="text" />
+          <ErrorMessage name="city" />
+        </FlexContainer>
+        <FlexContainer className="state">
+          <label htmlFor="state">Estado:</label>
+          <Field name="state" type="text" />
+          <ErrorMessage name="state" />
+        </FlexContainer>
+        <FlexContainer className="zipCode">
+          <label htmlFor="zipCode">CEP:</label>
+          <Field name="zipCode" type="text" />
+          <ErrorMessage name="zipCode" />
+        </FlexContainer>
+        <FlexContainer className="terms" row>
+          <StyledTerms>
+            <Field name="acceptedTerms" component="input" type="checkbox" />
+            <label htmlFor="acceptedTerms">Aceito os termos e condições</label>
+          </StyledTerms>
+          <ErrorMessage name="acceptedTerms" />
+        </FlexContainer>
+        <FlexContainer className="submit">
+          <Button margin="auto" type="submit">
+            Submit
+          </Button>
+        </FlexContainer>
+      </GridContainer>
+    </Form>
+  </Formik>
+);
 
 export default FormCustomer;
---
+++ src/components/FormCustomer/styles.js
@@ -0,0 +1,135 @@
+import styled, { css } from "styled-components";
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  input {
+    width: 100%;
+    border-radius: 0.5em;
+    border: 1px solid gray;
+  }
+
+  label {
+    color: gray;
+  }
+  ${(props) =>
+    props.row &&
+    css`
+      flex-direction: row;
+      input[type="checkbox"] {
+        margin: 0;
+        padding: 0;
+      }
+    `}
+`;
+
+export const Title = styled.h2``;
+
+export const DivBackDrop = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+`;
+
+export const GridContainer = styled.div`
+  display: grid;
+  position: fixed;
+  border-radius: 0.5em;
+  top: 2em;
+  left: 20em;
+  right: 20em;
+  padding: 0.7em;
+  z-index: 2;
+  background-color: #e3f5f7;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-areas:
+    "user user user"
+    "grid-one grid-one grid-one"
+    "grid-two grid-two grid-four"
+    "grid-five grid-six grid-seven"
+    "grid-eight . ."
+    "address address address"
+    "grid-nine grid-ten grid-eleven"
+    "grid-twelve grid-thirteen grid-fourteen"
+    "grid-fifteen grid-fifteen grid-fifteen"
+    ". center-button .";
+  grid-gap: 0.5em;
+
+  .title {
+    grid-area: user;
+  }
+  .name {
+    grid-area: grid-one;
+  }
+
+  .email {
+    grid-area: grid-two;
+  }
+
+  .cpf {
+    grid-area: grid-four;
+  }
+
+  .rg {
+    grid-area: grid-five;
+  }
+
+  .phone {
+    grid-area: grid-six;
+  }
+
+  .birthday {
+    grid-area: grid-seven;
+  }
+
+  .password {
+    grid-area: grid-eight;
+  }
+
+  .address {
+    grid-area: address;
+  }
+
+  .district {
+    grid-area: grid-nine;
+  }
+
+  .street {
+    grid-area: grid-ten;
+  }
+
+  .number {
+    grid-area: grid-eleven;
+  }
+
+  .city {
+    grid-area: grid-twelve;
+  }
+
+  .state {
+    grid-area: grid-thirteen;
+  }
+
+  .zipCode {
+    grid-area: grid-fourteen;
+  }
+
+  .terms {
+    grid-area: grid-fifteen;
+  }
+
+  .submit {
+    grid-area: center-button;
+  }
+`;
+
+export const StyledTerms = styled.div`
+  grid-gap: 0.5em;
+  display: grid;
+  grid-template-columns: 1em 14em;
+  align-items: center;
+`;
