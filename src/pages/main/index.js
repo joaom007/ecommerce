a6709ec8a655f3944604b745e1dd336bbe5c0ead
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2020-09-12 13:12:04 -0300

    Addition of the pagination into the main page

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Header, Footer, Button } from "../../components/index";
-import { Container, Product, Img, Figcaption, Flex, Price } from "./styles";
-import exemple from "../../assets/imagem-ficticia.jpg";
-import { products } from "./products";
+import React, { useState, useEffect } from 'react'
+import { Header, Footer, Button } from '../../components/index'
+import { Container, Product, Img, Figcaption, Flex, Price } from './styles'
+import exemple from '../../assets/imagem-ficticia.jpg'
+import data from './products'
 
 const handleProducts = ({ idProduct, color, description, unitaryValue }) => (
   <Product key={idProduct}>
@@ -13,18 +13,57 @@ const handleProducts = ({ idProduct, color, description, unitaryValue }) => (
       <Button>Comprar</Button>
     </Flex>
   </Product>
-);
+)
 
-const Main = () => (
-  <>
-    <Header />
-    <Container>{products.map(handleProducts)}</Container>
-    <Flex className="before-after">
-      <Button>Anterior</Button>
-      <Button>Próxima</Button>
-    </Flex>
-    <Footer />
-  </>
-);
+const Main = () => {
+  const [pagination, setPaginaton] = useState({
+    limit: 9,
+    start: 0,
+  })
+  const [products, setProducts] = useState([])
 
-export default Main;
+  function nextPage() {
+    setPaginaton({ ...pagination, start: pagination.limit + pagination.start })
+  }
+
+  function prevPage() {
+    setPaginaton({ ...pagination, start: pagination.limit - pagination.start })
+  }
+
+  function handlePagination() {
+    const { limit, start } = pagination
+
+    const paginatedProducts = data.filter((item, index) => {
+      if (index >= start && index < start + limit) {
+        return item
+      }
+    })
+
+    setProducts(paginatedProducts)
+  }
+
+  useEffect(() => {
+    handlePagination()
+  }, [pagination])
+
+  return (
+    <>
+      <Header />
+      <Container>{products.map(handleProducts)}</Container>
+      <Flex className="before-after">
+        <Button onClick={prevPage} disabled={pagination.start === 0}>
+          Anterior
+        </Button>
+        <Button
+          onClick={nextPage}
+          disabled={pagination.start + pagination.limit >= data.length}
+        >
+          Próxima
+        </Button>
+      </Flex>
+      <Footer />
+    </>
+  )
+}
+
+export default Main
