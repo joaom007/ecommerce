ff8d8954438614033aca8b5183629d328f7ddcb4
João Marcos <joaomarcossm2013@hotmail.com>
2020-09-21 00:05:16 -0300

    ajusted

---
+++ src/pages/main/products-images/index.js
@@ -0,0 +1,38 @@
+//Lavatório_Reto_Embutida
+export { default as lrea1e } from './Lavatorio_Reto_Embutida/lavatorio-reto-embutida-amarelo-estelar.jpg'
+export { default as lrea2e } from './Lavatorio_Reto_Embutida/lavatorio-reto-embutida-azul-estelar.jpg'
+export { default as lrebi } from './Lavatorio_Reto_Embutida/lavatorio-reto-embutida- branco-itaúnas.jpg'
+export { default as lrebp } from './Lavatorio_Reto_Embutida/lavatorio-reto-embutida-branco-prime.jpg'
+export { default as lrecm } from './Lavatorio_Reto_Embutida/lavatorio-reto-embutida-crema-marfil.jpg'
+export { default as lrepsg } from './Lavatorio_Reto_Embutida/lavatorio-reto-embutida-preto-são-gabriel.jpg'
+export { default as lret } from './Lavatorio_Reto_Embutida/lavatorio-reto-embutida-travertino.jpg'
+export { default as lrevu } from './Lavatorio_Reto_Embutida/lavatorio-reto-embutida-verde-ubatuba.jpg'
+export { default as lreve } from './Lavatorio_Reto_Embutida/lavatorio-reto-embutida-vermelho-estelar.jpg'
+
+//Lavatório_Saia_Esculpida
+export { default as bsea1e } from './Lavatorio_Saia_Esculpida/Bancada Saia Esculpida - Amarelo Estelar.jpg'
+export { default as bsea2e } from './Lavatorio_Saia_Esculpida/Bancada Saia Esculpida - Azul Estelar.jpg'
+export { default as bsebi } from './Lavatorio_Saia_Esculpida/Bancada Saia Esculpida - Branco Itaúnas.jpg'
+export { default as bsebp } from './Lavatorio_Saia_Esculpida/Bancada Saia Esculpida - Branco Prime.jpg'
+export { default as bsecm } from './Lavatorio_Saia_Esculpida/Bancada Saia Esculpida - Crema Marfil.jpg'
+export { default as bsemt } from './Lavatorio_Saia_Esculpida/Bancada Saia Esculpida - Mármore Travertino.jpg'
+export { default as bsesg } from './Lavatorio_Saia_Esculpida/Bancada Saia Esculpida - São Gabriel.jpg'
+export { default as bsevu } from './Lavatorio_Saia_Esculpida/Bancada Saia Esculpida - Verde Ubatuba.jpg'
+export { default as bseve } from './Lavatorio_Saia_Esculpida/Bancada Saia Esculpida - Vermelho Estelar.jpg'
+
+//Lavatório_Saia_Sobreposta
+export { default as bssa1e } from './Lavatorio_Saia_Sobreposta/Bancada Saia Sobreposta - Amarelo Estelar.jpg'
+export { default as bssa2e } from './Lavatorio_Saia_Sobreposta/Bancada Saia Sobreposta - Azul Estelar.jpg'
+export { default as bssbi } from './Lavatorio_Saia_Sobreposta/Bancada Saia Sobreposta - Branco Itaúnas.jpg'
+export { default as bssbp } from './Lavatorio_Saia_Sobreposta/Bancada Saia Sobreposta - Branco Prime.jpg'
+export { default as bsscm } from './Lavatorio_Saia_Sobreposta/Bancada Saia Sobreposta - Crema Marfil.jpg'
+export { default as bssmt } from './Lavatorio_Saia_Sobreposta/Bancada Saia Sobreposta - Mármore Travertino.jpg'
+export { default as bsssg } from './Lavatorio_Saia_Sobreposta/Bancada Saia Sobreposta - São Gabriel.jpg'
+export { default as bssvu } from './Lavatorio_Saia_Sobreposta/Bancada Saia Sobreposta - Verde Ubatuba.jpg'
+export { default as bssve } from './Lavatorio_Saia_Sobreposta/Bancada Saia Sobreposta - Vermelho Estelar.jpg'
+
+//Pia_Reto_Inox
+export { default as pirba } from './Pia_Reto_Inox/Pia 57 Reto - Bege Arabesco.jpg'
+export { default as pirbi } from './Pia_Reto_Inox/Pia 57 Reto - Branco Itaúnas.jpg'
+export { default as pirsg } from './Pia_Reto_Inox/Pia 57 Reto - São Gabriel.jpg'
+export { default as pirvu } from './Pia_Reto_Inox/Pia 57 Reto - Verde Ubatuba.jpg'
